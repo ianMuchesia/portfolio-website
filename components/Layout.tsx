043a89788465f4fa40cbfc1abbf723d7d818181a
ianMuchesia <ianmuchesia@yahoo.com>
2023-04-21 17:42:56 +0300

    navbar

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+
+
+interface LayoutProps {
+    children: ReactNode;
+  }
+
+  
+  const Layout = ({ children }: LayoutProps) => {
+    return (
+      <>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </>
+    );
+  };
+  
+  export default Layout;
---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { IoIosMenu} from "react-icons/io";
+const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+  return (
+    <header className={styles.header}>
+      <div className={styles.navCenter}>
+      <Image
+       src='/logo.png'
+       alt="logo"
+       width={100}
+       height={50}
+
+      />
+  <AiOutlineMenu className={styles.menuBar}  onClick={()=>setToggle(true)}/>
+      </div>
+    
+      <nav className={toggle?styles.nav:styles.hideLinks}>
+        <div className={styles.dropdownMenu}>
+          <h2>Welcome</h2>
+        <AiOutlineClose  className={styles.menuBar} onClick={()=>setToggle(false)}/>
+        </div>
+      
+        <ul className={styles.menuList}>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Portfolio</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
